cc0e157ecaad19bbe97a7588cb6ce93c07340077
OdolutionApps <83309784+Usamaiqbal789@users.noreply.github.com>
2022-08-13 11:39:46 -0700

    updated

--- insta_integrate/__manifest__.py
+++ insta_integrate/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name' : ' Instagram',
+    'name' : ' Instagram Integrate App',
     'version' : '1.1',
     'summary': 'Customization',
     'sequence': 10,
