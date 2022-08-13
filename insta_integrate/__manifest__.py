b850429920d08bea061a4d3fc7c1dc3c65d199fe
OdolutionApps <83309784+Usamaiqbal789@users.noreply.github.com>
2022-08-13 10:59:55 -0700

    updated

---
+++ insta_integrate/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : ' Instagram',
+    'version' : '1.1',
+    'summary': 'Customization',
+    'sequence': 10,
+    'description': """Customization in odoo modules  """,
+    'category': 'Extra Tools',
+    'license': 'LGPL-3',
+    'images' : [],
+    'depends' : ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+         'views/menu.xml',
+         'views/desk.xml',
+         'views/token.xml',
+         
+        ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
